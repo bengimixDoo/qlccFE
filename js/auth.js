d93768a7dd9eb1515dbd0fc9f2567828b04d303f
bengimixDoo <96771380+bengimixDoo@users.noreply.github.com>
2025-11-12 21:46:00 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,7 @@
  * URL CƠ SỞ CỦA API.
  * !!! THAY THẾ URL NÀY BẰNG URL TỪ NGROK CỦA BẠN !!!
  */
-const API_BASE_URL = "https://1c7df611cf01.ngrok-free.app"; // <--- THAY URL NÀY
+const API_BASE_URL = "https://ce9074ea072b.ngrok-free.app"; // <--- THAY URL NÀY
 
 // Chạy ngay khi DOM được tải
 document.addEventListener("DOMContentLoaded", () => {
